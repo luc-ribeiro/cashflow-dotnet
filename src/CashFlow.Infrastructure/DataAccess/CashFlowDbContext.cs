d7d68750a67ba6922b1866b6b56c963a498a2675
Lucas <lucasribeirodesouza@live.com>
2024-11-12 15:22:28 -0300

    feat: adding migration for tags

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -9,4 +9,10 @@ public class CashFlowDbContext : DbContext
 
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Tag>().ToTable("Tags");
+    }
 }
